b6952eea20951c4d7d1b32b30a6d44d140de0b12
Harsh <dangiharsh@gmail.com>
2025-02-15 15:40:03 +0000

    Added dynamic path detection for location of .env files
    When tests are executed via commands like - go test ./...
    go then changes directory to the directory containing the path, obviously .env file will then not be found.
    Thus, needed to dynamically figure out which directly the process currently is in and then create an absolute path to .env

--- config/config.go
+++ config/config.go
@@ -3,12 +3,20 @@ package config
 import (
 	"fmt"
 	"os"
+	"path/filepath"
+	"regexp"
 
 	"github.com/joho/godotenv"
 )
 
+var projectDirName = "go-rest-api-with-fiber"
+
 func Config(key string) string {
-	err := godotenv.Load("../.env")
+	reg := regexp.MustCompile(`^(.*` + projectDirName + `)`)
+	cwd, _ := os.Getwd()
+	rootPath := reg.Find([]byte(cwd))
+	err := godotenv.Load(filepath.Join(string(rootPath), `.env`))
+
 	if err != nil {
 		fmt.Print("Error loading. env file")
 	}
